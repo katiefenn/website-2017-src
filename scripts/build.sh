9cecb4962cf2cdc442b54b510f58e84c670094d2
Katie Fenn <katie.fenn@googlemail.com>
2017-01-22 19:47:43 +0000

    Fixed function name in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ function doBuild {
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
-    doCompile
+    doBuild
     exit 0
 fi
 
